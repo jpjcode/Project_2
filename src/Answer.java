88929149854eb00fc931205874b6a1dbf0e9e684
Andrew Larrazabal <a_larrazabal@massbay.edu>
2026-02-23 23:05:02 -0500

    Changes to Main, Answers and PuzzleInfo

--- src/Answer.java
+++ src/Answer.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Answer {
 
     private Grid grid;
@@ -5,34 +7,45 @@ class Answer {
     private Grid grid3;
 
 
-    public Answer(Grid grid, Grid grid2, Grid grid3) {
-        this.grid = grid;
-        this.grid2 = grid2;
-        this.grid3 = grid3;
+    public Answer(String s) {
+        Scanner scnr = new Scanner(s);
+        int gridSize = scnr.nextInt();
+        scnr.nextLine();
+        this.grid = new Grid(gridSize, gridSize, scnr.nextLine());
+        this.grid2 = new Grid(gridSize, gridSize, scnr.nextLine());
+        this.grid3 = new Grid(gridSize, gridSize, scnr.nextLine());
     }
+
     public boolean compareCell(Answer other, int i, int j) {
         return grid.getGridCell(i, j).equals(other.getGrid().getGridCell(i, j)) &&
                grid2.getGridCell(i, j).equals(other.getGrid2().getGridCell(i, j)) &&
                grid3.getGridCell(i, j).equals(other.getGrid3().getGridCell(i, j));
     }
+
     public void setGrid(Grid grid) {
         this.grid = grid;
     }
+
     public void setGrid2(Grid grid2) {
         this.grid2 = grid2;
     }
+
     public void setGrid3(Grid grid3) {
         this.grid3 = grid3;
     }
+
     public Grid getGrid() {
         return grid;
     }
+
     public Grid getGrid2() {
         return grid2;
     }
+
     public Grid getGrid3() {
         return grid3;
     }
+
     public String toString() {
         String string = "";
         string += "Grid 1:\n" + grid.toString() + "\n";
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@
 //Edited by Andrew Larrazabal and Lior Sapir
 
 import java.awt.FlowLayout;
-import java.awt.Desktop.Action;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -13,10 +10,14 @@ import javax.swing.JFrame;
 
 class Main {
     public static void main(String[] args) throws FileNotFoundException {
-		File infoFile = new File("src\\puzzle1.csv");
+		File infoFile = new File("C:\\Users\\Larra\\Desktop\\CS225\\Project_2\\src\\puzzle1.csv");
 		PuzzleInfo info = new PuzzleInfo(infoFile);
-		Grid testGrid = new Grid(4, 4, "3 2 1 0");
+		Grid testGrid = new Grid(info.getCandidates()[0].length, info.getCandidates()[0].length, "3 2 1 0");
+		Answer testAnswer = new Answer("4\n0 1 2 3\n0 1 2 3\n 0 1 2 3");
+		PuzzleUI p = new PuzzleUI(info);
+		TabMenu t = new TabMenu();
 
+		System.out.println("Test Grid:\n");
 		for (int i = 0; i < testGrid.getGridHeight(); i++) {
 			for (int j=0; j < testGrid.getGridWidth(); j++) {
 				System.out.print(testGrid.getGridCell(j, i).getState());
@@ -24,20 +25,45 @@ class Main {
 			System.out.println();
 		}
 
+		System.out.println();
+
+		System.out.println("Test Answer Grid 1:\n");
+		for (int i = 0; i < testAnswer.getGrid().getGridHeight(); i++) {
+            for (int j=0; j < testGrid.getGridWidth(); j++) {
+                System.out.print(testAnswer.getGrid().getGridCell(j, i).getState());
+            }
+            System.out.println();
+        }
+
+		System.out.println();
+
+		System.out.println("Test Answer Grid 2:\n");
+		for (int i = 0; i < testAnswer.getGrid2().getGridHeight(); i++) {
+            for (int j=0; j < testGrid.getGridWidth(); j++) {
+                System.out.print(testAnswer.getGrid2().getGridCell(j, i).getState());
+            }
+            System.out.println();
+        }
+
+		System.out.println();
+
+		System.out.println("Test Answer Grid 3:\n");
+		for (int i = 0; i < testAnswer.getGrid3().getGridHeight(); i++) {
+            for (int j=0; j < testGrid.getGridWidth(); j++) {
+                System.out.print(testAnswer.getGrid3().getGridCell(j, i).getState());
+        	}
+            System.out.println();
+        }
+
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 0));
 		
-		PuzzleUI p = new PuzzleUI(info);
-		TabMenu t = new TabMenu();
 		frame.add(p);
 		frame.add(t);		
 		frame.pack();
 		frame.setVisible(true);
 
 		JButton test = new JButton();
-		
-
-
     }
 }
